ebfd7fd0ecff8be7d57475f755b58af2bbc29882
Fazlul Kabir Shohag <linkvisionsoft@yahoo.com>
2020-10-17 12:22:02 +0600

    task added

--- src/features/task/controlles/task/task.controller.ts
+++ src/features/task/controlles/task/task.controller.ts
@@ -1,6 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { TaskService } from '../../services/TaskService';
 import { AwesomeController } from '../../../../core/base/controller/AwesomeController';
+import { TaskDTO } from '../../dto/task.dto';
 
 @Controller('task')
 export class TaskController extends AwesomeController{
@@ -8,5 +9,20 @@ export class TaskController extends AwesomeController{
             private taskService: TaskService
       ) {
             super()
+      }
+
+      @Get()
+      findAllAction() {
+            return this.taskService.findAll()
+      }
+
+      @Post()
+      createAction(@Body(ValidationPipe) task: TaskDTO) {
+            return this.taskService.createNewAction(task)
+      }
+
+      @Get('delete')
+      deeleteAction() {
+            return this.taskService.clear()
       }
 }
